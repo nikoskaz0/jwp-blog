4a70fbabcad1f98d7e762e5f210f158931609e69
gracefulBrown <sunghyun.ryu@woowahan.com>
2019-07-01 00:13:34 +0900

    step1

--- src/main/java/com/techcourse/myblog/PageController.java
+++ src/main/java/com/techcourse/myblog/PageController.java
@@ -1,7 +1,22 @@
 package com.techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/articles")
+    public String articles() {
+        return "index";
+    }
+
+    @GetMapping("/writing")
+    public String writing() {
+        return "article-edit";
+    }
 }
