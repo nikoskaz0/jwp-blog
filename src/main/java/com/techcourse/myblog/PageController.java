5e1ab450ea29d69ea60dd25143a4e4629511d55c
gracefulBrown <sunghyun.ryu@woowahan.com>
2019-07-01 00:20:33 +0900

    step2

--- src/main/java/com/techcourse/myblog/PageController.java
+++ src/main/java/com/techcourse/myblog/PageController.java
@@ -1,12 +1,14 @@
 package com.techcourse.myblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
     @GetMapping("/")
-    public String index() {
+    public String index(String blogName, Model model) {
+        model.addAttribute("blogName", blogName);
         return "index";
     }
 
